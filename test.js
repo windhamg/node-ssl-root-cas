182abd0929162530cf84da3e6c90ed44565f6bb5
Paul Spicer <pspicer@prevalent.net>
2016-12-23 13:33:10 -0500

    Updated tests to run latest() as a promise first and check return values (also forces download if the latest file already existed)

--- test.js
+++ test.js
@@ -1,12 +1,22 @@
 'use strict';
 
-var invigilate = require('invigilate'),
+var Promise = require('bluebird'),
+    invigilate = require('invigilate'),
     log = invigilate(module);
 
 invigilate.loggers.default = console;
 
-testDefault();
+testLatest();
 
+function testLatest () {
+    var rootCasLatest = require('.').latest();
+    var latest = require('./latest').latest();
+    if (rootCasLatest !== latest) throw new Error('should have returned the same promise');
+    Promise.join(rootCasLatest, latest, function (latest1, latest2) {
+        if (latest1 !== latest2) throw new Error('should have returned the same list');
+        testDefault();
+    })
+}
 // backwards compat
 function testDefault() {
     require('https').globalAgent.options.ca = null;

5835635c291b167a726583cffff7e9c594fdc95d
Paul Spicer <pspicer@prevalent.net>
2016-12-23 13:30:32 -0500

    Made decorator methods non-enumerable, added injected key to check (but not set) injected status. Also added latest to the “constructor” so that every instance has it

--- index.js
+++ index.js
@@ -4,36 +4,68 @@ var path = require('path'),
     latestFile = path.join(storesDir, 'ssl-root-cas-latest'),
     originalFile = path.join(storesDir, 'ssl-root-cas'),
     CaStore = require('ca-store'),
-    originalStore = RootCas(CaStore.load(originalFile));
+    originalStore = RootCas(CaStore.load(originalFile)),
+    generating = false;
 
 originalStore.latestFile = latestFile;
-originalStore.latest = function () { return CaStore.generate(latestFile); };
 
 module.exports = originalStore;
 
 function RootCas (list) {
     var rootCas = (list || []).slice(0),
-        injected = false,
+        injected = !!(list && list.injected),
         httpsOpts = https.globalAgent.options;
     
-    rootCas.inject = function inject () {
-        if (injected) return rootCas;
+    return Object.defineProperties(rootCas, {
+        injected: {
+            configurable: false,
+            enumerable: false,
+            get: function getInjected () { return injected; },
+            set: function setInjected () { return injected; }
+        },
+        inject: {
+            configurable: false,
+            enumerable: false,
+            writable: false,
+            value: function inject () {
+                if (injected) return rootCas;
         
-        httpsOpts.ca = (httpsOpts.ca || []).concat(rootCas);
-        injected = true;
+                httpsOpts.ca = (httpsOpts.ca || []).concat(rootCas);
+                injected = true;
         
-        return rootCas;
-    };
-    
-    rootCas.addCert = function (cert) {
-        rootCas.push(cert);
-        if (injected) httpsOpts.ca.push(cert);
-        return rootCas;
-    };
-    
-    rootCas.create = function (list) {
-        return RootCas(list || rootCas);
-    };
-    
-    return rootCas;
+                return rootCas;
+            }
+        },
+        addCert:{
+            configurable: false,
+            enumerable: false,
+            writable: false,
+            value: function addCert (cert) {
+                rootCas.push(cert);
+                if (injected) httpsOpts.ca.push(cert);
+                return rootCas;
+            }
+        },
+        create:{
+            configurable: false,
+            enumerable: false,
+            writable: false,
+            value: function create (list) {
+                return RootCas(list || rootCas);
+            }
+        },
+        latest: {
+            configurable: false,
+            enumerable: false,
+            writable: false,
+            value: function () {
+                if (generating) return generating;
+                generating = CaStore.generate(latestFile).then(function (latest) {
+                    generating = false;
+                    return RootCas(latest);
+                });
+                return generating;
+            }
+        }
+    });
 }

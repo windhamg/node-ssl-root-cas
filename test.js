c22bcfe83190f396cff3c5733103caa95109c188
Paul Spicer <pspicer@prevalent.net>
2016-12-27 17:08:31 -0500

    Added tests for deprecation, added failing exit code to test process

--- test.js
+++ test.js
@@ -2,65 +2,74 @@
 
 var Promise = require('bluebird'),
     invigilate = require('invigilate'),
+    path = require('path'),
+    https = require('https'),
+    request = Promise.promisifyAll(require('request')),
     log = invigilate(module);
 
 invigilate.loggers.default = console;
 
-testLatest();
+Promise.try(() => testLatest()).then(success).catch(error);
+
+function success (retval) {
+    log.info(retval);
+    process.exit(0);
+}
+
+function error (err) {
+    log.error(err.stack);
+    process.exit(1);
+}
 
 function testLatest () {
     var rootCasLatest = require('.').latest();
     var latest = require('./latest').latest();
     if (rootCasLatest !== latest) throw new Error('should have returned the same promise');
-    Promise.join(rootCasLatest, latest, function (latest1, latest2) {
+    return Promise.join(rootCasLatest, latest, function (latest1, latest2) {
         if (latest1 !== latest2) throw new Error('should have returned the same list');
-        testDefault();
+        return testDefault();
     })
 }
 // backwards compat
 function testDefault() {
-    require('https').globalAgent.options.ca = null;
-    var rootCas = require('./latest');
-    var request = require('request');
+    https.globalAgent.options.ca = require('.');
     
-    require('https').globalAgent.options.ca = rootCas;
-    request.get('https://daplie.com/404.html', function (err, resp, body) {
-        if (err) {
-            throw err;
-        }
-        
-        testInject();
-    });
+    return request.getAsync('https://daplie.com/404.html').then(testInject);
 }
 
 function testInject() {
-    require('https').globalAgent.options.ca = null;
-    var rootCas = require('./latest').inject();
-    var request = require('request');
+    https.globalAgent.options.ca = null;
+    require('./latest').inject();
     
-    require('https').globalAgent.options.ca = rootCas;
-    request.get('https://daplie.com/404.html', function (err, resp, body) {
-        if (err) {
-            throw err;
-        }
-        
-        testCreate();
-    });
+    return request.getAsync('https://daplie.com/404.html').then(testAddFile);
+}
+
+function testDeprecation () {
+    process.throwDeprecation = true;
+    try {
+        require('./latest').create([]).addFile(path.join(__dirname, 'stores', 'pems'));
+    }
+    catch (err) {
+        process.throwDeprecation = false;
+        return testAddFile();
+    }
+    throw new Error('should have thrown an error for the deprecated addFile() function');
+}
+
+function testAddFile () {
+    https.globalAgent.options.ca = null;
+    require('./latest').create([]).addFile(path.join(__dirname, 'stores', 'pems')).inject();
+    return request.getAsync('https://daplie.com/404.html').then(testCreate);
 }
 
 function testCreate() {
-    require('https').globalAgent.options.ca = null;
-    var rootCas = require('./latest').create();
-    var request = require('request');
+    https.globalAgent.options.ca = null;
+    require('./latest').create().inject();
     
-    require('https').globalAgent.options.ca = rootCas;
-    request.get('https://daplie.com/404.html', function (err, resp, body) {
-        if (err) {
-            throw err;
-        }
-        
-        log.info(body);
-    });
+    return Promise.fromNode(cb => request.get('https://daplie.com/404.html', function (err, resp, body) {
+        if (err) return cb(err);
+        cb(null, body);
+    }));
 }
 
 // TODO test with a company certificate
